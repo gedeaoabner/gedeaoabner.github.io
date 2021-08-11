883ad100066b9ea63db668c52674c13f83581a89
Abner <gedeaoabner@gmail.com>
2021-08-11 03:11:53 -0300

    cria pagina home - incompleto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <section
+        className="section-home"
+      >
+        <section className="center-home container-lg">
+          <span className="my-name">
+            Gedeão Abner Dos Santos
+          </span>
+          <hr />
+          <span className="my-role">
+            Programador
+          </span>
+        </section>
+      </section>
+    );
+  }
+}
+
+export default Home;
